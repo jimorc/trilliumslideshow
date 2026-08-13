71bbf6872c0aaf9c30f654b1c6fc63f6235ea24f
Jim Orcheson <jimorcheson@gmail.com>
2026-08-13 07:28:18 -0400

    Add pub mod config to force test discovery

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+pub mod config;
 pub mod gui;
 
+//use config::default_values::DefaultValues;
 use gui::main_frame::MainFrame;
 
 fn main() {
+    //    let defaults = DefaultValues::new();
+
     let _ = wxdragon::main(|_| {
         let main_frame = MainFrame::new();
         main_frame.show();
